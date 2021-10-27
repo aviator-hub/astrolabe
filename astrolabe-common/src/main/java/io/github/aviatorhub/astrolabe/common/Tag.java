043b743f3112528b0a3e277355e8bc7a5dde72d9
jie.mei <meijie.work@gmail.com>
2021-10-27 14:11:43 +0800

    project init

---
+++ astrolabe-common/src/main/java/io/github/aviatorhub/astrolabe/common/Tag.java
@@ -0,0 +1,14 @@
+package io.github.aviatorhub.astrolabe.common;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tag {
+  private String identify;
+  private String description;
+}
---
+++ astrolabe-table/src/main/java/io/github/aviatorhub/astrolabe/DataSource.java
@@ -0,0 +1,8 @@
+package io.github.aviatorhub.astrolabe;
+
+public class DataSource {
+
+  private String identify;
+  private String address;
+  private String description;
+}
